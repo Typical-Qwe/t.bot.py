0011addfe338d3d3efebb9aad5817a46b6fe9db4
Typical-Qwe <petranata2018@gmail.com>
2025-06-08 15:39:57 +0200

    Update logic.py

--- logic.py
+++ logic.py
@@ -1,12 +1,11 @@
 import telebot
-from logic import gen_pass, gen_emodji, flip_coin  # Импортируем функции из bot_logic
 from config import token
-# Замени 'TOKEN' на токен твоего бота
+
 bot = telebot.TeleBot(token)
 
 @bot.message_handler(commands=['start'])
 def send_welcome(message):
-    bot.reply_to(message, "Привет! Я твой Telegram бот. Напиши команду /hello, /bye, /pass, /emodji или /coin  ")
+    bot.reply_to(message, "Привет! Я твой Telegram бот по экологии. Напиши команду /hello, /bye, /statistic, /country_status или /advices  ")
 
 @bot.message_handler(commands=['hello'])
 def send_hello(message):
@@ -16,20 +15,19 @@ def send_hello(message):
 def send_bye(message):
     bot.reply_to(message, "Пока! Удачи!")
 
-@bot.message_handler(commands=['pass'])
+@bot.message_handler(commands=['statistic'])
 def send_password(message):
-    password = gen_pass(10)  # Устанавливаем длину пароля, например, 10 символов
-    bot.reply_to(message, f"Вот твой сгенерированный пароль: {password}")
+    statistic = "https://tochno.st/problems/ecology"
+    bot.reply_to(message, f"Вот статистика по экологии на сегодняшний день: {statistic}")
 
-@bot.message_handler(commands=['emodji'])
+@bot.message_handler(commands=['ecology_status'])
 def send_emodji(message):
-    emodji = gen_emodji()
-    bot.reply_to(message, f"Вот эмоджи': {emodji}")
+    country_status = "https://nonews.co/directory/lists/countries/ecology"
+    bot.reply_to(message, f"Вот таблица статуса каждой страны в мире': {country_status}")
 
-@bot.message_handler(commands=['coin'])
+@bot.message_handler(commands=['advices'])
 def send_coin(message):
-    coin = flip_coin()
-    bot.reply_to(message, f"Монетка выпала так: {coin}")
+    advices = "https://rusarctica.ru/blog/idei-i-layfkhaki/kak-spasti-prirodu-8-shagov-kotorye-mozhet-sdelat-kazhdyy/"
+    bot.reply_to(message, f"Вот список советов на улучщение экологии мира: {advices}")
 
-# Запускаем бота
 bot.polling()
